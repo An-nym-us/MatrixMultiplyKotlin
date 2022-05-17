864514230fc6324d8f8693ec19521a35909deb66
Jonathan <jlgunderson2@gmail.com>
2022-05-17 11:57:33 -0600

    650651

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -23,11 +23,11 @@ fun takeObject(o: SomeObj) {
 
 fun create_2D_Matrix(size_of_Matrix: Int)
 {
-
+    val currentTimestamp = System.currentTimeMillis();
 
     //assert(size_of_Matrix != 0);
 
-    var rnds : Double = 0.0;
+    var rnds : Double = .5;
         // Create a 2D Array
     for (i in 0..size_of_Matrix) {
         var array = arrayOf<Double>();
@@ -35,12 +35,12 @@ fun create_2D_Matrix(size_of_Matrix: Int)
         var array_output = arrayOf<Double>();
         for (j in 0..size_of_Matrix) {
             // Create a random Double IN range of -1.0 amd 1.0
-            rnds = ThreadLocalRandom.current().nextDouble(-1.0, 1.0);
-            array += rnds;
+            //rnds = ThreadLocalRandom.current().nextDouble(-1.0, 1.0);
+            array += .5
 
             // Fill array 2
-            rnds = ThreadLocalRandom.current().nextDouble(-1.0, 1.0);
-            array_2 += rnds;
+            //rnds = ThreadLocalRandom.current().nextDouble(-1.0, 1.0);
+            array_2 += .5
 
             array_output += 0.0;
 
@@ -51,13 +51,21 @@ fun create_2D_Matrix(size_of_Matrix: Int)
     }
 
 
+    val EndTimestamp = System.currentTimeMillis();
 
 
+    var timeout = EndTimestamp - currentTimestamp ;
+    println("$timeout  In miliseconds")
 }
 
 
 fun multiply_The_Arrays(size_of_Matrix: Int)
 {
+    val currentTimestamp = System.currentTimeMillis();
+
+
+
+
 
     // Vomit 2D array
     /*
@@ -84,6 +92,9 @@ for (array in output_array_Seq) {
     println();
 }
 */
+
+
+
     println("====================================================================================");
     for (i in 0 .. size_of_Matrix)
     {
@@ -92,6 +103,7 @@ for (array in output_array_Seq) {
             for (k in 0.. size_of_Matrix )
             {
                 output_array_Seq[i][j] += randomArray_1[i][k] * randomArray_2[k][j];
+                //println(output_array_Seq[j][k]);
             };
         };
     };
@@ -101,11 +113,34 @@ for (array in output_array_Seq) {
     {
         for (k in 0.. size_of_Matrix )
         {
-            println(output_array_Seq[j][k]);
+           //println(output_array_Seq[j][k]);
             size += 1;
         };
     };
+
+
+    val EndTimestamp = System.currentTimeMillis();
+
+
+    var timeout = EndTimestamp - currentTimestamp ;
+
     println(size)
+
+    println("$timeout  In miliseconds")
+    /*
+    if (timeout < 1000)
+    {
+        println("$timeout  In miliseconds")
+    }
+    else
+    {
+        timeout /= 1000;
+        println("$timeout  In Seconds")
+    }
+*/
+
+
+
 }
 
 
@@ -115,10 +150,13 @@ for (array in output_array_Seq) {
 fun main(args: Array<String>) {
     println("Hello World!");
 
-    val matrix_Size: Int = 10
+    val matrix_Size: Int = 1000
     create_2D_Matrix(matrix_Size);
-
+    println("Matrixs have been cvreated")
     multiply_The_Arrays(matrix_Size);
+
+
+
 
     /*
     var passing: Double = 1000.0;
